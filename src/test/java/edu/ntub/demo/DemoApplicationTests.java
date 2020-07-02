0644ea6aae484c9583f79d9c20c3ab3c88a35470
Billy Huang <10646003@ntub.edu.tw>
2020-07-02 17:14:24 +0800

    Initial project

---
+++ src/test/java/edu/ntub/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
